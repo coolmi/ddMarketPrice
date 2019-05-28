7639bd16ad52c64c819f4772329c84bf5443ecac
yuemin1.zhang <1061920403@qq.com>
2019-05-28 14:01:50 +0800

    提交页面1

--- xdyz/src/api/baseConfig.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zym on 19/5/27.
- * 部署去掉切换用户和接口地址
- */
-const baseConfig = {
-  baseURL: 'http://10.5.1.253:10061' // 测试机
-}
-
-export default baseConfig
--- xdyz/src/lib/FlowError.js
+++
@@ -1,8 +0,0 @@
-class FlowError {
-  constructor(msg = '错误', funcName = '', action = '') {
-    this.msg = msg
-    this.funcName = funcName
-    this.action = action
-  }
-}
-export default FlowError
